a640514cbb39daf9f736d421a827939c50438ec5
Jonathan Raposo <jonathan.raposo.luna@gmail.com>
2024-01-23 02:11:30 -0500

    modify

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+
 require("dotenv").config();
 
 // ℹ️ Connects to the database
 require("./db");
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
+
 const express = require("express");
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+// ℹ️ run middleware
 require("./config")(app);
 
-// 👇 Start handling routes here
+//routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
@@ -32,7 +30,7 @@ app.use("/", userRoutes)
 
 
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+// ❗ To handle errors. 
 require("./error-handling")(app);
 
 module.exports = app;
